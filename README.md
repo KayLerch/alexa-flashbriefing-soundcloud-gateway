93f3280863924f5eea1e5fcab36567ceb99d7bf1
kaylerch <dekstar@gmx.de>
2017-07-22 20:47:08 +0200

    Cut off trailing feed items to avoid outdated feeds in your Flash-Briefing

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# alexa-flashbriefing-soundcloud-gateway
-This gateway-service pulls a Soundcloud-RSS-feed and converts audio-feed-urls to HTTPS in order
-to comply with SSL requirements for audio-feeds for Alexa Flash-Briefing skills. 
+### Alexa Flash-Briefing Gateway-Service for Soundcloud Podcasts
+This gateway-service pulls your Soundcloud-RSS-feed and converts audio-feed-urls to HTTPS in order
+to comply with SSL requirements for audio-feeds for Alexa Flash-Briefing skills. Secondly, the script
+also picks the first configurable X items of your Soundcloud feed and returns it for your Flash-Briefing by
+cutting off the trailing (likely outdated) feed items.
 
-1) Go to handler.js and enter your Soundcloud user-id.
-2) Deploy the Serverless-stack with "serverless deploy" in your CLI
-3) Use the AWS API-Gateway endpoint you'll find in the console output once your stack is deployed and use it as your Flash-Briefing skill-endpoint.
+Before you start, make sure you configured your Podcast's RSS feed in Soundcloud ([Howto](https://help.soundcloud.com/hc/en-us/articles/115003570048-Setting-up-your-podcast-s-RSS-feed))
+
+1. Go to handler.js and enter your Soundcloud user-id.
+
+2. Optionally, adjust the max number of feeds from Soundcloud you want to return to Alexa
+
+3. Deploy the Serverless-stack with "serverless deploy" in your CLI (don't forget to set AWS credentials in your environment)
+
+4. Use the AWS API-Gateway endpoint you'll find in the console output once your stack is deployed and use it as your Flash-Briefing skill-endpoint.
--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 'use strict';
 const https = require('https');
-const scuid = '31xxxxx62';
+// your soundcloud user-id goes here
+const scuid = '316xxxx62';
+// max number of feed items to return (please note it will at least be one no matter if you set this parameter below 1)
+const items_to_return = 1;
 
 module.exports.feed = (event, context, callback) => {
     var payload = '';
@@ -10,11 +13,23 @@ module.exports.feed = (event, context, callback) => {
             payload += chunk;
         });
         response.on('end', function () {
-            console.log('Got response: ' + payload);
+            // split into array of items
+            var chunks = payload.replace(/url=\"http\:/gi,'url=\"https:').split('</item>');
+            // where first item is contained in the first chunk and will always be part of the return
+            // while splitting by the item-end-tag it got lost so we have to append it again
+            var body = chunks[0] + '</item>';
+            // go through rest of the items and append until desired num of items or number of items returned is reached
+            // ignore the last chunk as it contains the tail-payload we will append later on
+            for(var i = 1; i < Math.min(chunks.length - 1, items_to_return); i++){
+                // while splitting by the item-end-tag it got lost so we have to append it again
+                body += (chunks[i] + '</item>');
+            }
+            // finally, append the tail payload
+            body += chunks.slice(-1)[0];
             callback(null, {
                 statusCode: response.statusCode,
                 headers: response.headers,
-                body: payload.replace(/url=\"http\:/gi,'url=\"https:')
+                body: body
             });
         });
     }).on('error', function(e) {
